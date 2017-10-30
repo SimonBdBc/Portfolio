2c3dcb738158cb2c5e2a6ef2b4c082c34e9bb21c
SimonBdBc <simonbdbc@gmail.com>
2017-10-31 00:59:42 +0100

    add navbar php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once('./templates/head.html');
 require_once('./templates/foot.html');
 require_once('./pages/header.php');
-require_once('./pages/main.php');
+require_once('./pages/skill.php');
+require_once('./pages/xp.php');
 require_once('./pages/footer.php');
  ?>
