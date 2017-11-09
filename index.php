8ca6b50ae570963509dc1c48b2042808f95db0f0
SimonBdBc <simonbdbc@gmail.com>
2017-11-09 01:27:09 +0100

    add page projet

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ require_once('./templates/foot.html');
 require_once('./pages/header.php');
 require_once('./pages/skill.php');
 require_once('./pages/xp.php');
+require_once('./pages/work.php');
 require_once('./pages/footer.php');
  ?>
--- pages/header.php
+++ pages/header.php
@@ -8,6 +8,7 @@
       <li><a href="#top" id="accueil" class="a-no-style"><h4> Accueil </h4><i class="fa fa-angle-up fa-lg" aria-hidden="true"></i></a></li>
       <li><a href="#part1" id="skill" class="a-no-style"><h4> Compétences </h4><i class="fa fa-hand-spock-o" aria-hidden="true"></i></a></li>
       <li><a href="#part2" id="xp" class="a-no-style"><h4> Parcours Pro </h4><i class="fa fa-user-o" aria-hidden="true"></i></a></li>
+      <li><a href="#part3" id="work" class="a-no-style"><h4> Projets </h4><i class="fa fa-laptop" aria-hidden="true"></i></a></li>
       <li><a href="#bottom" id="contact" class="a-no-style"><h4> Contact </h4><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
     </ul>
   </div>
